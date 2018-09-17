344c39a3092c8871cb7e9e816b6e1a075271dddf
Chris Cheshire <chrift@users.noreply.github.com>
2018-09-17 14:33:51 +0100

    Remove unneccessary refreshing of token in root route

--- demo/routes/root.js
+++ demo/routes/root.js
@@ -8,12 +8,6 @@ module.exports = [
     handler: async function (request, h) {
       const { idm } = request.server.methods
 
-      const creds = await idm.getCredentials(request)
-
-      if (creds && creds.isExpired()) {
-        await idm.refreshToken(request)
-      }
-
       return h.view('index', {
         user: null,
         idm,
